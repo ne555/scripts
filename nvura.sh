9a176e7221cbdc09ac75ff79083cfc722f649404
Aldo Jirdeb <fake@address.com>
2022-10-16 23:49:25 -0300

    full path socket to avoid difference zathura/nvim

--- nvura.sh
+++ nvura.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #export $(dbus-launch)
-zathura --synctex-editor-command="nvr --servername $1-socket -c \"let g:syncpdf='$1'\" --remote -c %{line} %{input}" $1 &>error.log &
+socket_name="/tmp/nvimsocket-$1"
+zathura --synctex-editor-command="nvr --servername ${socket_name} -c \"let g:syncpdf='$1'\" --remote -c %{line} %{input}" $1 &>error.log &
 reset
-nvim --listen "$1-socket" -c "let g:syncpdf='$1'"
+nvim --listen "${socket_name}" -c "let g:syncpdf='$1'"
